80974374f5d35d7bad41cc121986588cbae7370a
MagicalBrain <13542593649@163.com>
2021-06-28 08:47:06 +0800

    update from my macbook air

---
+++ C++/C++11/code/pair-inVector.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+
+using namespace std;
+using std::vector;
+using std::string;
+
+int main() {
+    string str;
+    int num;
+    vector<pair<string, int> > v;
+
+    while (cin >> str >> num)
+        v.push_back(make_pair(str, num));
+    
+    for (auto i : v)
+    {
+        cout << i.first << " " << i.second ;
+    }
+    cout << endl;  
+    
+    return 0;
+}
