a9ce68e2a0f78d1eac142072c9fa82e51214b9e5
MagicalBrain <13542593649@163.com>
2021-07-14 23:55:22 +0800

    update from mac

--- Cmake/demo4Cmake/boost-test-example/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hrl/demo4Cmake/boost-test-example
-# Build directory: /home/hrl/demo4Cmake/boost-test-example/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(boost_test "/home/hrl/demo4Cmake/boost-test-example/build/cpp_test")
-set_tests_properties(boost_test PROPERTIES  _BACKTRACE_TRIPLES "/home/hrl/demo4Cmake/boost-test-example/CMakeLists.txt;31;add_test;/home/hrl/demo4Cmake/boost-test-example/CMakeLists.txt;0;")
