595f9bfbfee118685fc00bd65633e04b84ebc767
MagicalBrain <13542593649@163.com>
2021-07-30 21:21:48 +0800

    add Plugin from O15

---
+++ 显扬科技/HYPluginDemo/HRL-Plugin/AlgorithmDemo.cpp
@@ -0,0 +1,32 @@
+﻿#include "AlgorithmDemo.h"
+
+#include <iostream>
+
+#include <algorithm/VoxelGridFilter.hpp>
+#include <common/TimeHelper.h>
+#include <common/LoggerHelper.hpp>
+
+using namespace hy;
+
+int AlgorithmDemo::param(const std::string& file)
+{
+	std::cout << file << std::endl;
+	return 0;
+}
+
+int AlgorithmDemo::compute(int index, bool force, int max)
+{
+	std::cout << data_->Clouds.size() << std::endl;
+	TimeHelper helper;
+	
+	VoxelGridFilter::Filter<pcl::PointXYZ>(data_->Clouds.front(), 5);
+	/// xx
+	/// xxx 
+	/// PPF
+	/// ....
+	/// kkkk................
+	/// 
+	data_->Results.resize(max);
+	HY_LOG_SEV(info) << boost::format("xxxx %1% ms") % helper.toc();
+	return 0;
+}
