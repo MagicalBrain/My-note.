4b5f8057e0ca072aa6e4e5867d58631cefabc09f
374870361@qq.com <374870361@qq.com>
2021-08-19 20:27:05 +0800

    update C++ with Date from O15

--- C++/C++如何获取日期.md
+++ C++/C++如何获取日期.md
@@ -1,6 +1,8 @@
 # C++如何获取日期
 
 使用跨平台的通用代码：
+使用`currentDateTime()`函数，代码如下：
+
 @import "./code/getDate.cpp"
 
 
--- C++/code/getDate.cpp
+++ C++/code/getDate.cpp
@@ -18,6 +18,15 @@ const string currentDateTime() {
     return buf;
 }
 
+void test() {
+    std::time_t t = std::time(0);   // get time now
+    std::tm* now = std::localtime(&t);
+    std::cout << (now->tm_year + 1900) << '-' 
+         << (now->tm_mon + 1) << '-'
+         <<  now->tm_mday
+         << "\n";
+}
+
 int main() {
     cout << "Date: " << currentDateTime() << endl;
     return 0;
