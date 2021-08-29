da4108695b5f1dc445a5a325547e96d504804a58
374870361@qq.com <374870361@qq.com>
2021-08-29 23:03:33 +0800

    update C++ the beautiful of progamming from G15

--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,3 @@
 git push gitee main
-git config --global user.name "MagicalBrain"
-git config --global user.email "13542593649@163.com"
+
 git push github main
-git config --global user.name "374870361@qq.com"
-git config --global user.email "374870361@qq.com"
