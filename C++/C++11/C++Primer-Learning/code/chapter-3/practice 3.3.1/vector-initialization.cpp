ff4a41fc6914e005f6f993a093dcb1f4ad0ee723
374870361@qq.com <374870361@qq.com>
2021-11-03 19:56:02 +0800

    update C++ vector. from O15

--- C++/C++11/C++Primer-Learning/code/chapter-3/practice 3.3.1/vector-initialization.cpp
+++ C++/C++11/C++Primer-Learning/code/chapter-3/practice 3.3.1/vector-initialization.cpp
@@ -20,5 +20,12 @@ int main() {
     vector<int> v5{10, 42};
     vector<string> v6{10};
     vector<string> v7{10, "hi"};
+
+    // 我自己的测试
+    std::cout << "v1.empty() is: " << v1.empty() << std::endl;
+
+    std::cout << "v2.empty() is: " << v2.empty() << std::endl;
+    v2.clear();
+    std::cout << "after clear v2.empty() is: " << v2.empty() << std::endl;
     return 0;
 }
