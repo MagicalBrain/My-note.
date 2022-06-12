fafb5c49e2acf2ef47f43292c42565b8e5d0b289
MB's X13 <374870361@qq.com>
2022-06-13 00:54:05 +0800

    update

--- Win10/BatCmd批处理文件/code/AddInMYSDKROOT.bat
+++ Win10/BatCmd批处理文件/code/AddInMYSDKROOT.bat
@@ -7,6 +7,6 @@ rem setx /M MYSDK_ROOT C:\hrl\
 set cpplib=C:\hrl\MyCPP\PTA-Basic
 
 ::添加变量进MYSDK_ROOT变量
-setx /m "MYSDK_ROOT" "%cpplib%;%MYSDK_ROOT%"
+setx /m "MYSDK_ROOT" "%MYSDK_ROOT%;%cpplib%"
 
 pause
