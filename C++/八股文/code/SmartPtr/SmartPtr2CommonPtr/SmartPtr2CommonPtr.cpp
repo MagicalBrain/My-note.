3b02c8bc4849bb2684a925275ca38001d4016ede
MB's X13 <374870361@qq.com>
2022-07-14 10:57:51 +0800

    update smart ptr

--- C++/八股文/code/SmartPtr/SmartPtr2CommonPtr/SmartPtr2CommonPtr.cpp
+++ C++/八股文/code/SmartPtr/SmartPtr2CommonPtr/SmartPtr2CommonPtr.cpp
@@ -2,10 +2,63 @@
 //
 
 #include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+/// @brief shared_ptr 转成 普通指针
+void shared_ptr2cptr() {
+    std::cout << "start transfer smartptr to common ptr!\n";
+
+    std::shared_ptr<std::string> str_ptr1 = std::make_shared<std::string>("this is a string.");
+    std::string* str_cptr1 = str_ptr1.get();
+
+    std::vector<std::string*> str_cptrs;
+
+    str_cptrs.push_back(str_cptr1);
+    //str_cptrs.push_back(str_ptr1);
+
+    try
+    {
+        std::cout << str_cptrs.size() << std::endl;
+        std::cout << str_cptrs[0]->c_str() << std::endl;
+    }
+    catch (const std::exception& err)
+    {
+        std::cout << err.what() << std::endl;
+    }
+}
+
+/// @brief 普通指针转换成shared_ptr
+void cptr2shared_ptr() {
+    try
+    {
+        std::shared_ptr<std::string> str_sptr;
+
+        std::string* str_ptr = new std::string("a string");
+
+        str_sptr.reset(str_ptr);
+
+        std::cout << *str_sptr << std::endl;
+
+        // 注意要和new成对使用
+        delete str_ptr;
+    }
+    catch (const std::exception& err)
+    {
+        std::cout << err.what() << std::endl;
+    }
+    
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "start transfer program!\n";
+    
+    //shared_ptr2cptr();
+    cptr2shared_ptr();
+
+    return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
